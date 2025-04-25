266b4a0f9522a46e00840f69fae27be474cd1470
Javier Zapata <zapatajavier34@gmail.com>
2025-04-25 14:08:07 -0500

    Se corrigen errores al dejar solo las tareas de firebase y se agrega estado de carga inicial

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,7 @@ import FooterJavi from "./components/footer.jsx";
 import Tarea from "./components/tarea.jsx";
 
 function App() {
-    /* Definimos la estructura de las tareas y creamos algunas de Ejemplo */
-
-    /* Sin local storage */
-    const [tareas, setTareas] = useState();
-
-    /* Con local storage */
-    /* const [tareas, setTareas] = useState(() => {
-        const savedTareas = localStorage.getItem('tareas');
-        return savedTareas ? JSON.parse(savedTareas) : tareasIniciales;
-    }); */
-
-    /* Cargamos las tareas iniciales de la aplicación con la base de datos desde firebase */
-    useEffect(() => {
-        const fetchTareas = async () => {
-            const tareasCollection = collection(db, "tareas");
-            const tareasSnapshot = await getDocs(tareasCollection);
-            const tareasList = tareasSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            setTareas(tareasList);
-        };
     
-        fetchTareas();
-    }, []);
-
     /* Definimos los responsables de las tareas */
     const responsables = ["Javier Zapata", "Jhonatan Rojas", "Michael Martinez"];
 
@@ -38,7 +16,29 @@ function App() {
     const [nuevoResponsable, setNuevoResponsable] = useState('');
     const [nuevoLink, setNuevoLink] = useState('');
     const [nuevaFechaEntrega, setNuevaFechaEntrega] = useState('');
+    const [cargando, setCargando] = useState(true); /* Definimos un estado inicial para cargar las tareas desde FireBase */
+    const [tareas, setTareas] = useState([]); /* Sin local storage */
     const inputRef = useRef(null);
+    
+    /* Con local storage */
+    /* const [tareas, setTareas] = useState(() => {
+        const savedTareas = localStorage.getItem('tareas');
+        return savedTareas ? JSON.parse(savedTareas) : tareasIniciales;
+    }); */
+
+    /* Cargamos las tareas iniciales de la aplicación con la base de datos desde firebase */
+    useEffect(() => {
+        const fetchTareas = async () => {
+            setCargando(true);
+            const tareasCollection = collection(db, "tareas");
+            const tareasSnapshot = await getDocs(tareasCollection);
+            const tareasList = tareasSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setTareas(tareasList);
+            setCargando(false);
+        };
+    
+        fetchTareas();
+    }, []);
 
     /* Guardamos las tareas en el localStorage cada vez que cambian */
     useEffect(() => {
@@ -135,116 +135,125 @@ function App() {
         <>
             {/* Contenedor principal */}
             <h1 className="titulo">Pendientes UT - The Police</h1>
-            {/* Contenedor lista de tareas */}
-            <div className="todolist">
-                {/* Contenedor de tareas pendientes */}
-                <div className="listaNoDone">
-                    <h2>Lista de Tareas</h2>
-                    <ul>
-                        {tareas
-                            .filter(tarea => !tarea.completado)
-                            .map(tarea => (
-                                <Tarea 
-                                    key = { tarea.id } 
-                                    tarea = { tarea } 
-                                    toggleCompletado = { toggleCompletado }
-                                    eliminarTarea = { eliminarTarea }
-                                />
-                            ))}
-                    </ul>
+            { cargando ? (
+                <div className="cargando">
+                    <p>Cargando Tareas ...</p>
                 </div>
-                {/* Contenedor de tareas completadas */}
-                <div className="listaDone">
-                    {tareasCompletadas.length > 0 && (
-                        <>
-                            <h2>Tareas Completadas</h2>
+            ) : (
+                <>
+                    {/* Contenedor lista de tareas */}
+                    <div className="todolist">
+                        {/* Contenedor de tareas pendientes */}
+                        <div className="listaNoDone">
+                            <h2>Lista de Tareas</h2>
                             <ul>
-                                {tareasCompletadas.map(tarea => (
-                                    <Tarea 
-                                        key = { tarea.id } 
-                                        tarea = { tarea } 
-                                        toggleCompletado = { toggleCompletado }
-                                        eliminarTarea = { eliminarTarea }
-                                    />
-                                ))}
+                                {tareas
+                                    .filter(tarea => !tarea.completado)
+                                    .map(tarea => (
+                                        <Tarea 
+                                            key = { tarea.id } 
+                                            tarea = { tarea } 
+                                            toggleCompletado = { toggleCompletado }
+                                            eliminarTarea = { eliminarTarea }
+                                        />
+                                    ))}
                             </ul>
-                        </>
-                    )}
-                </div>
-            </div>
-            {/* Contenedor de botones */}
-            <div className="buttons">
-                <button 
-                    className = "añadirTarea" 
-                    onClick = { () => setModalVisible(true) }
-                    >
-                        Añadir Tarea
-                </button>
-                <button 
-                    className = "borrarTarea" 
-                    onClick = { async () => {
-                        // Elimina todas las tareas de Firestore
-                        const tareasCollection = collection(db, "tareas");
-                        const tareasSnapshot = await getDocs(tareasCollection);
-                        const batch = tareasSnapshot.docs.map(doc => deleteDoc(doc.ref));
-
-                        // Espera a que todas las tareas sean eliminadas
-                        await Promise.all(batch);
-
-                        // Limpia el estado local
-                        setTareas([]);
-                    }}
-                    >
-                    Borrar todas las tareas
-                </button>
-            </div>
-            {/* Ventana de añadir tarea */}
-            {modalVisible && (
-                <div className="modal" onKeyDown={handleKeyDown} onClick={handleOutsideClick} tabIndex="0">
-                    <div className="modal-content">
-                        <h3>Nueva Tarea</h3>
-                        {/* Descripción de la tarea */}
-                        <input 
-                            type="text" 
-                            value={nuevaTareaTexto} 
-                            onChange={(e) => setNuevaTareaTexto(e.target.value)} 
-                            onKeyPress={handleKeyPress}
-                            placeholder="Descripción de la tarea" 
-                            ref={inputRef}
-                        />
-                        {/* Responsable de la tarea */}
-                        <select
-                            value = {nuevoResponsable}
-                            onChange = { (e) => setNuevoResponsable(e.target.value) } 
-                            >
-                            <option value="" disabled>Selecciona un responsable</option>
-                            { responsables.map((responsable, index) => (
-                                <option key={index} value={responsable}>
-                                    {responsable}
-                                </option>
-                            ))}
-                        </select>
-                        {/* URL del trabajo */}
-                        <input 
-                            type="url" 
-                            value={nuevoLink} 
-                            onChange={(e) => setNuevoLink(e.target.value)} 
-                            placeholder="Enlace (opcional)"
-                        />
-                        {/* Fecha entrega */}
-                        <input 
-                            type="date" 
-                            value={nuevaFechaEntrega} 
-                            onChange={(e) => setNuevaFechaEntrega(e.target.value)} 
-                            placeholder="Fecha de entrega"
-                        />
-                        <button onClick={agregarTarea}>Agregar</button>
-                        <button onClick={() => setModalVisible(false)}>Cancelar</button>
+                        </div>
+                        {/* Contenedor de tareas completadas */}
+                        <div className="listaDone">
+                            {tareasCompletadas.length > 0 && (
+                                <>
+                                    <h2>Tareas Completadas</h2>
+                                    <ul>
+                                        {tareasCompletadas.map(tarea => (
+                                            <Tarea 
+                                                key = { tarea.id } 
+                                                tarea = { tarea } 
+                                                toggleCompletado = { toggleCompletado }
+                                                eliminarTarea = { eliminarTarea }
+                                            />
+                                        ))}
+                                    </ul>
+                                </>
+                            )}
+                        </div>
                     </div>
-                </div>
-            )}
-            {/* Pie de pagina */}
-            <FooterJavi />
+                    {/* Contenedor de botones */}
+                    <div className="buttons">
+                        <button 
+                            className = "añadirTarea" 
+                            onClick = { () => setModalVisible(true) }
+                            >
+                                Añadir Tarea
+                        </button>
+                        <button 
+                            className = "borrarTarea" 
+                            onClick = { async () => {
+                                // Elimina todas las tareas de Firestore
+                                const tareasCollection = collection(db, "tareas");
+                                const tareasSnapshot = await getDocs(tareasCollection);
+                                const batch = tareasSnapshot.docs.map(doc => deleteDoc(doc.ref));
+
+                                // Espera a que todas las tareas sean eliminadas
+                                await Promise.all(batch);
+
+                                // Limpia el estado local
+                                setTareas([]);
+                            }}
+                            >
+                            Borrar todas las tareas
+                        </button>
+                    </div>
+                    {/* Ventana de añadir tarea */}
+                    {modalVisible && (
+                        <div className="modal" onKeyDown={handleKeyDown} onClick={handleOutsideClick} tabIndex="0">
+                            <div className="modal-content">
+                                <h3>Nueva Tarea</h3>
+                                {/* Descripción de la tarea */}
+                                <input 
+                                    type="text" 
+                                    value={nuevaTareaTexto} 
+                                    onChange={(e) => setNuevaTareaTexto(e.target.value)} 
+                                    onKeyPress={handleKeyPress}
+                                    placeholder="Descripción de la tarea" 
+                                    ref={inputRef}
+                                />
+                                {/* Responsable de la tarea */}
+                                <select
+                                    value = {nuevoResponsable}
+                                    onChange = { (e) => setNuevoResponsable(e.target.value) } 
+                                    >
+                                    <option value="" disabled>Selecciona un responsable</option>
+                                    { responsables.map((responsable, index) => (
+                                        <option key={index} value={responsable}>
+                                            {responsable}
+                                        </option>
+                                    ))}
+                                </select>
+                                {/* URL del trabajo */}
+                                <input 
+                                    type="url" 
+                                    value={nuevoLink} 
+                                    onChange={(e) => setNuevoLink(e.target.value)} 
+                                    placeholder="Enlace (opcional)"
+                                />
+                                {/* Fecha entrega */}
+                                <input 
+                                    type="date" 
+                                    value={nuevaFechaEntrega} 
+                                    onChange={(e) => setNuevaFechaEntrega(e.target.value)} 
+                                    placeholder="Fecha de entrega"
+                                />
+                                <button onClick={agregarTarea}>Agregar</button>
+                                <button onClick={() => setModalVisible(false)}>Cancelar</button>
+                            </div>
+                        </div>
+                    )}
+                    {/* Pie de pagina */}
+                    <FooterJavi />
+                </>
+            ) }
+            
         </>
     );
 };
